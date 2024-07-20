47de0cd2e97d4dabbded888f11c4856d7d6b9b75
cnintcheu <cnintcheu@gmail.com>
2024-07-20 18:42:41 -0400

    Cross Orgin Request Problem. Need fix

--- powerpoint-tutorial-yo/My Office Add-in/src/taskpane/taskpane.js
+++ powerpoint-tutorial-yo/My Office Add-in/src/taskpane/taskpane.js
@@ -6,6 +6,194 @@
 /* global document, Office */
 
 import { base64Image } from "../../base64Image";
+
+// The initialize or onReady function is required for all add-ins.
+Office.initialize = function (reason) {
+
+  // Checks for the DOM to load using the jQuery ready method.
+  $(document).ready(async () => {
+
+    // Run sendFile when Submit is clicked.
+    $('#submit').on("click", async () => {
+      await sendFile();
+    });
+
+    // Update status.
+    await updateStatus("Ready to send file.");
+  });
+}
+
+
+
+
+// Get all of the content from a PowerPoint or Word document in 100-KB chunks of text.
+async function sendFile() {
+
+  // https://learn.microsoft.com/en-us/javascript/api/office/office.document?view=common-js-preview#office-office-document-getfileasync-member(1)
+  // Office.FileType.Compressed ("compressed"),
+  // Office.FileType.PDF ("pdf"),
+  // Office.FileType.Text ("text")
+
+  Office.context.document.getFileAsync("compressed",
+    { sliceSize: 100000 },
+    async (result) => {
+
+      if (result.status === Office.AsyncResultStatus.Succeeded) {
+
+        // Get the File object from the result.
+        var myFile = result.value;
+        var state = {
+          file: myFile,
+          counter: 0,
+          sliceCount: myFile.sliceCount
+        };
+
+        await updateStatus("Getting file of " + myFile.size + " bytes");
+        await getSlice(state);
+      } else {
+        await updateStatus(result.status);
+      }
+    });
+}
+
+// Get a slice from the file and then call sendSlice.
+async function getSlice(state) {
+  state.file.getSliceAsync(state.counter, async (result) => {
+    if (result.status == Office.AsyncResultStatus.Succeeded) {
+      await updateStatus("Sending piece " + (state.counter + 1) + " of " + state.sliceCount);
+      await sendSlice(result.value, state);
+    } else {
+      await updateStatus(result.status);
+    }
+  });
+}
+
+
+async function sendSlice2(slice, state) {
+  var data = slice.data;
+
+  // If the slice contains data, create an HTTP request.
+  if (data) {
+
+    // Encode the slice data, a byte array, as a Base64 string.
+    // NOTE: The implementation of myEncodeBase64(input) function isn't
+    // included with this example. For information about Base64 encoding with
+    // JavaScript, see https://developer.mozilla.org/docs/Web/JavaScript/Base64_encoding_and_decoding.
+    //var fileData = myEncodeBase64(data);
+    var fileData = bytesToBase64(data);
+
+
+    // Create a new HTTP request. You need to send the request
+    // to a webpage that can receive a post.
+    var request = new XMLHttpRequest();
+
+    // Create a handler function to update the status
+    // when the request has been sent.
+    request.onreadystatechange = async () => {
+      if (request.readyState == 4) {
+
+        await updateStatus("Sent " + slice.size + " bytes.");
+        state.counter++;
+
+        if (state.counter < state.sliceCount) {
+          await getSlice(state);
+        } else {
+          await closeFile(state);
+        }
+      }
+    }
+
+    //NEED ENDPOINT
+    request.open("POST", "https://studious-space-system-g47q46w467wcp445-3000.app.github.dev/data");
+    request.setRequestHeader("Slice-Number", slice.index);
+
+    // Send the file as the body of an HTTP POST
+    // request to the web server.
+    request.send(fileData);
+  }
+}
+
+
+async function sendSlice(slice, state) {
+  var data = slice.data;
+
+  // If the slice contains data, create an HTTP request.
+  if (data) {
+
+    // Encode the slice data, a byte array, as a Base64 string.
+    // NOTE: The implementation of myEncodeBase64(input) function isn't
+    // included with this example. For information about Base64 encoding with
+    // JavaScript, see https://developer.mozilla.org/docs/Web/JavaScript/Base64_encoding_and_decoding.
+    //var fileData = myEncodeBase64(data);
+    var fileData = bytesToBase64(data);
+    const requestUrl = 'https://studious-space-system-g47q46w467wcp445-3000.app.github.dev/data';
+
+
+    $.ajax({
+      url: requestUrl,
+      type: 'POST',
+      headers: { 'Slice-Number': slice.index },
+      processData: false,
+      contentType: 'application/octet-stream',
+      data: fileData
+    }).done(async (response) => {
+
+      console.log("response POST", response);
+      if (response) {
+        await updateStatus("Sent " + slice.size + " bytes.");
+        state.counter++;
+
+        if (state.counter < state.sliceCount) {
+          await getSlice(state);
+        } else {
+          await closeFile(state);
+        }
+      }
+
+    }).fail(async (error) => {
+      console.log(" Error AJAX", error);
+      console.error(error);
+    });
+
+  }
+}
+
+
+async function closeFile(state) {
+  // Close the file when you're done with it.
+  state.file.closeAsync(async (result) => {
+
+    // If the result returns as a success, the
+    // file has been successfully closed.
+    if (result.status === Office.AsyncResultStatus.Succeeded) {
+      await updateStatus("File closed.");
+    } else {
+      await updateStatus("File couldn't be closed.");
+    }
+  });
+}
+
+
+async function dataUrlToBytes(dataUrl) {
+  const res = await fetch(dataUrl);
+  return new Uint8Array(await res.arrayBuffer());
+}
+
+function bytesToBase64(bytes) {
+  const binString = Array.from(bytes, (byte) =>
+    String.fromCodePoint(byte),
+  ).join("");
+  return btoa(binString);
+}
+
+// Create a function for writing to the status div.
+async function updateStatus(message) {
+  var statusInfo = $('#status');
+  statusInfo[0].innerHTML += message + "<br/>";
+}
+
+
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint) {
     document.getElementById("sideload-msg").style.display = "none";
@@ -56,7 +244,7 @@ function getSlideMetadata() {
     }
   });
 }
-  
+
 async function addSlides() {
   await PowerPoint.run(async function (context) {
     context.presentation.slides.add();
@@ -121,10 +309,10 @@ async function tryCatch(callback) {
 }
 
 
- async function detectSensitiveData() {
-  
+async function detectSensitiveData() {
+
   // This example gets the body of the item as plain text.
-  await PowerPoint.run(async  (context) => {
+  await PowerPoint.run(async (context) => {
 
 
     context.presentation.load("slides");
@@ -139,44 +327,43 @@ async function tryCatch(callback) {
 
     //let body = context.document.body;
 
-      //if (result.status == "succeeded") {
+    //if (result.status == "succeeded") {
 
-        const patterns = {
-          ssn: /\b(?!000|666|9\d{2})([0-8]\d{2}|7([0-6]\d|7[012]))([-]?)\d{2}\3\d{4}\b/,
-          creditCard: /\b(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})\b|\b(?:(?:4[0-9]{3}|5[1-5][0-9]{2}|6[0-9]{3}|3[47][0-9]{2})[- ]?[0-9]{4}[- ]?[0-9]{4}[- ]?[0-9]{4})\b/,
-          dateOfBirth: /\b(0[1-9]|1[0-2])[/-](0[1-9]|[12]\d|3[01])[/-](19|20)\d{2}\b/,
-          email: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/,
-          phoneNumber: /\b(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b/,
-          ipAddress: /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/,
-          passportNumber: /\b[A-Z]{1,2}[0-9]{6,9}\b/,
-          driverLicense: /\b[A-Z]{1}[0-9]{5,7}\b/,
-          bankAccount: /\b[0-9]{8,17}\b/
-        };
+    const patterns = {
+      ssn: /\b(?!000|666|9\d{2})([0-8]\d{2}|7([0-6]\d|7[012]))([-]?)\d{2}\3\d{4}\b/,
+      creditCard: /\b(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})\b|\b(?:(?:4[0-9]{3}|5[1-5][0-9]{2}|6[0-9]{3}|3[47][0-9]{2})[- ]?[0-9]{4}[- ]?[0-9]{4}[- ]?[0-9]{4})\b/,
+      dateOfBirth: /\b(0[1-9]|1[0-2])[/-](0[1-9]|[12]\d|3[01])[/-](19|20)\d{2}\b/,
+      email: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/,
+      phoneNumber: /\b(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b/,
+      ipAddress: /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/,
+      passportNumber: /\b[A-Z]{1,2}[0-9]{6,9}\b/,
+      driverLicense: /\b[A-Z]{1}[0-9]{5,7}\b/,
+      bankAccount: /\b[0-9]{8,17}\b/
+    };
 
-        const sensitiveInfo = {};
-        let text = result.value;
+    const sensitiveInfo = {};
+    let text = result.value;
 
-        for (const [type, pattern] of Object.entries(patterns)) {
-          const matches = text.match(pattern);
-          if (matches) {
-            sensitiveInfo[type] = matches.map(match => ({
-              value: match,
-              index: text.indexOf(match)
-            }));
-            addInformational(type.toUpperCase() + " detected as sensitive data");
-            showError(type.toUpperCase() + ": " + sensitiveInfo[type][0].value);
-            break;
-          }
-        }
+    for (const [type, pattern] of Object.entries(patterns)) {
+      const matches = text.match(pattern);
+      if (matches) {
+        sensitiveInfo[type] = matches.map(match => ({
+          value: match,
+          index: text.indexOf(match)
+        }));
+        addInformational(type.toUpperCase() + " detected as sensitive data");
+        showError(type.toUpperCase() + ": " + sensitiveInfo[type][0].value);
+        break;
+      }
+    }
 
 
 
-      //} else {
-        //addError('The content of your email is not accessible for policies control.')
+    //} else {
+    //addError('The content of your email is not accessible for policies control.')
 
-      //}
+    //}
 
-    });
-
+  });
 
 }
